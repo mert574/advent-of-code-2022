8eadfb28aa87f8239c2f51f6a9a706a21418038b
Mert Yildiz <mert.yildiz@heyjobs.de>
2022-12-04 20:56:22 +0100

    day-03: fix indentation.

--- day-03/index.js
+++ day-03/index.js
@@ -4,15 +4,15 @@ import readInput from "../lib/readInput.js"
 
 export function part1(input) {
   return input.split("\n")
-  // split into two
-  .map((it) => [it.slice(0, it.length / 2), it.slice(-it.length / 2)])
-  // get intersection of characters
-  .flatMap(([bag1, bag2]) => bag1.split("").intersection(bag2).uniq())
-  // get char codes and normalize them by subtracting "a"
-  .map(char => charCodeOf(char) - charCodeOf("a") + 1)
-  // now [A-Z] is between (-31)-(-6) and [a-z] between 1-26. need to fix [A-Z]
-  .map(score => score > 0 ? score : (score + 58))
-  .sum()
+    // split into two
+    .map((it) => [it.slice(0, it.length / 2), it.slice(-it.length / 2)])
+    // get intersection of characters
+    .flatMap(([bag1, bag2]) => bag1.split("").intersection(bag2).uniq())
+    // get char codes and normalize them by subtracting "a"
+    .map(char => charCodeOf(char) - charCodeOf("a") + 1)
+    // now [A-Z] is between (-31)-(-6) and [a-z] between 1-26. need to fix [A-Z]
+    .map(score => score > 0 ? score : (score + 58))
+    .sum()
 }
 
 export function part2(input) {
