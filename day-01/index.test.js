0b1e8dd8a72b1522c72a5a0042d28eee7b84e197
Mert Yildiz <mert.yildiz@heyjobs.de>
2022-12-04 03:07:44 +0100

    day 01: simplify tests.

--- day-01/index.test.js
+++ day-01/index.test.js
@@ -3,24 +3,17 @@ import readInput from "../lib/readInput";
 
 describe('Day 1: Calorie Counting', () => {
   const example = readInput("example.txt")
+  const input = readInput("input.txt")
 
   describe('Part 1: How many total Calories is that Elf carrying?', () => {
-    it('works with the example', () => {
-      expect(part1(example)).toBe(24_000)
-    });
+    it('works with the example', () => expect(part1(example)).toBe(24_000));
 
-    it('works with the real input', () => {
-      expect(part1(readInput())).toBe(67_622)
-    });
+    it('works with the real input', () => expect(part1(input)).toBe(67_622));
   });
 
   describe('Part 2: How many Calories are those Elves carrying in total?', () => {
-    it('works with the example', () => {
-      expect(part2(example)).toBe(45_000)
-    });
+    it('works with the example', () => expect(part2(example)).toBe(45_000));
 
-    it('works with the real input', () => {
-      expect(part2(readInput())).toBe(20_1491)
-    });
+    it('works with the real input', () => expect(part2(input)).toBe(201_491));
   })
 });
